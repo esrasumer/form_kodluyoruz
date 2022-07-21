d661dc60bdebcaa4b64e76e38f12cd21a95eece0
esrasumer <esrasumer28@gmail.com>
2022-07-21 23:42:07 +0300

    css ekledim.

--- form/src/component/Contact/List/index.jsx
+++ form/src/component/Contact/List/index.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
-function List() {
+import {useState} from 'react'
+function List({contact}) {
+  const[filterText,setFilterText] = useState("");
+
+  const filtered = contact.filter((item)=>{
+    return Object.keys(item).some((key)=>{
+return item[key].toString().toLowerCase().includes(filterText.toLocaleLowerCase());
+    });
+  });
+
+  console.log("filtered: ",filtered);
+
   return (
-    <div></div>
+    <div>
+<input 
+  id='fılterContactInput' 
+  placeholder='Fılter Contact'
+  value={filterText}
+  onChange={(e) => setFilterText(e.target.value)} 
+  />
+
+      <ul>
+       { filtered.map((contact,i) =>(
+        <li key={i}>{contact.fullname}</li>
+       )
+       )
+       }
+{/*        
+       { contact.map((contact,i)=>(
+        <li key={i}>{contact.phone_number}</li>
+       ))
+       } 
+       */}
+      </ul>
+    </div>
   )
 }
 
--- form/src/component/Contact/index.jsx
+++ form/src/component/Contact/index.jsx
@@ -3,15 +3,31 @@ import Form from './Form';
 import List from './List';
 
 function Contact() {
-const [contact, setContact] = useState([]);
+const [contact, setContact] = useState([
+  {
+  fullname:"Mehmet",
+   phone_number:"09864",
+},
+{
+  fullname:"Esra",
+  phone_number:"123445",
+},
+{
+  fullname:"Esma",
+  phone_number:"065436",
+},
+
+]);
+
+
 
 useEffect(() => {
   console.log(contact);
 }, [contact])
 
-  return (    <div>
+  return (    <div id='container'>
         <Form addContact={setContact} contact={contact} />
-        <List />
+        <List contact={contact} />
         </div>  
   )
 }
